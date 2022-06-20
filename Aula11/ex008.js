597c6ac8afafef4d13a715a0ed7fa90c40a4d902
rodfernando <hazardrodrigo@hotmail.com>
2022-06-19 22:02:14 -0300

    updating

---
+++ Aula11/ex008.js
@@ -0,0 +1,8 @@
+console.log('O console executou normalmente apertando F8.')
+
+var vel = 4
+if (vel >= 60) {
+    console.log(`A velocidade foi de ${vel}km/h\nVocê foi multado!`)
+} else {
+    console.log(`Sua velocidade foi de ${vel}.\nUse sempre cinto de segurança!`)
+}
